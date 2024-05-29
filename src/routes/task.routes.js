bcedbf5dd0d59271b811f12a2ad890faaace8ef8
Matthew Rivero <matt@MacBook-Air-de-indira.local>
2024-05-29 02:32:34 +0200

    Fourth session

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,8 +1,17 @@
 import {Router} from 'express';
 import { authRequired } from '../middlewares/validateToken.js';
+import { getTasks, getTask, createTask, deleteTask, updateTask } from '../controllers/task.controller.js'
 
 const router = Router();
 
-router.get("/tasks", authRequired, (req, res) => { res.send("tasks") })
+router.get("/tasks", authRequired, getTasks)
+
+router.post("/tasks", authRequired, createTask)
+
+router.delete("/tasks/:id", authRequired, deleteTask)
+
+router.get("/tasks/:id", authRequired, getTask)
+
+router.put("/tasks/:id", authRequired, updateTask)
 
 export default router;
