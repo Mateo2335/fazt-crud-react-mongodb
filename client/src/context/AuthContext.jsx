823eb71f82be96598bf8abaae1f87d5980cedbfd
Matthew <mateoo2335@gmail.com>
2024-05-31 19:11:40 +0200

    Eighth session

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -41,6 +41,12 @@ export const AuthProvider = ({children}) => {
         }
     }
 
+    const logout = () => {
+        Cookies.remove("token");
+        setIsAuthenticated(false);
+        setUser(null)
+    }
+
     useEffect(()=> {
         if(errors.length > 0) {
             const timer = setTimeout(() => {
@@ -84,6 +90,7 @@ export const AuthProvider = ({children}) => {
         <AuthContext.Provider value={{
             signup,
             signin,
+            logout,
             loading,
             user,
             isAuthenticated,
--- client/src/pages/TaskPage.jsx
+++ client/src/pages/TaskPage.jsx
@@ -1,12 +1,24 @@
-import { useAuth } from "../context/AuthContext";
+import { useEffect } from "react";
+import { useTasks } from "../context/TasksContext";
+import TaskCard from '../components/TaskCard'
 
 function TaskPage() {
 
-    const {user} = useAuth();
+    const {getTasks, tasks} = useTasks();
 
-    console.log(user)
+    useEffect(() => {
+        getTasks()
+    }, [])
 
-    return ( <div>TaskPage</div> );
+    if (tasks.length === 0) return (<h1>No tasks</h1>)
+
+    return ( 
+        <div className="grid sd:grid-cols-2 md:grid-cols-3 gap-2">
+            {tasks.map(task => (
+                <TaskCard task={task} key={task._id}/>
+        ) )}
+        </div>
+    );
 }
 
 export default TaskPage;
