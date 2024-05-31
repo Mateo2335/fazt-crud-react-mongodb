da44b89ed400d7c3aacbff46904be56c17523a43
Matthew <mateoo2335@gmail.com>
2024-05-31 20:01:30 +0200

    Deploying

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.routes.js';
 
@@ -8,16 +9,24 @@ import cookieParser from 'cookie-parser';
 
 import cors from 'cors';
 
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'http://fazt-crud.matthewrivero.com'
 }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', authRoutes);
 app.use('/api', taskRoutes);
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
 
 export default app;
